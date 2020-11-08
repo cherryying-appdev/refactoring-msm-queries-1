917ea8da4fbd731da73a66fdc69d1b7bceba7212
yue-cherry-ying <cherryying@uchicago.edu>
2020-11-08 16:22:34 +0000

    finished with the assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,23 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def movie
+    m_id = self.movie_id
+
+    matching_movies = Movie.where({ :id => m_id })
+
+    the_movie = matching_movies.at(0)
+
+    return the_movie
+  end
+
+  def director
+    d_id = self.movie.director_id
+
+    matching_directors = Director.where({ :id => d_id })
+    
+    the_director = matching_directors.at(0)
+
+    return the_director
+  end
 end
